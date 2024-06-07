b90d7963724c247092bc473d61f984e2c5275417
James Moran <james.moranjr@yahoo.com>
2024-06-07 07:22:33 -0400

    mining animation

--- Assets/Scripts/Models/PathNode.cs
+++ Assets/Scripts/Models/PathNode.cs
@@ -61,6 +61,7 @@ public class PathNode : MonoBehaviour
     {
         var startingCredits = currentCredits;
         int amountMined = (currentCredits - mineValue) >= 0 ? mineValue : startingCredits;
+        StartCoroutine(GameManager.i.FloatingTextAnimation($"Mined {amountMined}", transform));
         currentCredits -= amountMined;
         mineralText.text = $"{currentCredits}";
         hasBeenMinedThisTurn = true;
